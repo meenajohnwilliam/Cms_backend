c21712b2177850e77612e6c755395950839b7311
loedroptaxi-del <leodroptaxi@gmail.com>
2026-08-11 17:43:55 +0530

    4nd commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const cors = require("cors");
 const superAdminRoutes = require("./src/routes/superAdmin.routes");
 const authRoutes = require("./src/routes/auth.routes")
 const planRoutes = require("./src/routes/plan.routes");
