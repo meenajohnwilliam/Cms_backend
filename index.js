40d011467d0dd19cf947ca72df91ec7f0ca0239b
loedroptaxi-del <leodroptaxi@gmail.com>
2026-08-11 17:36:42 +0530

    4nd commit

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express")
 const app = express()
-app.use(express.json())
 const superAdminRoutes = require("./src/routes/superAdmin.routes");
 const authRoutes = require("./src/routes/auth.routes")
 const planRoutes = require("./src/routes/plan.routes");
 
+app.use(express.json())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      // "https://your-frontend.netlify.app",
+    ],
+    credentials: true,
+  })
+);
 
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1/super-admin", superAdminRoutes);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/plans", planRoutes);
