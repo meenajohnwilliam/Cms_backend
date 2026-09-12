aee3370c63405208577a8520a68811c7345483fe
loedroptaxi-del <leodroptaxi@gmail.com>
2026-09-12 10:29:05 +0530

    new-db-37

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -898,13 +898,13 @@ const razorpayWebhook = async (req, res) => {
     }
 
     if (event === "subscription.authenticated") {
-      const startDate = new Date(
-        subscriptionData.current_start * 1000
-      );
+      // const startDate = new Date(
+      //   subscriptionData.current_start * 1000
+      // );
     
-      const endDate = new Date(
-        subscriptionData.current_end * 1000
-      );
+      // const endDate = new Date(
+      //   subscriptionData.current_end * 1000
+      // );
     
       // Cancel old active subscription
       await prisma.subscription.updateMany({
@@ -927,21 +927,21 @@ const razorpayWebhook = async (req, res) => {
         },
         data: {
           status: "ACTIVE",
-          startDate,
-          endDate,
+          // startDate,
+          // endDate,
         },
       });
     }
 
     // 3. Subscription Activated
     if (event === "subscription.activated") {
-      const startDate = new Date(
-        subscriptionData.current_start * 1000
-      );
+      // const startDate = new Date(
+      //   subscriptionData.current_start * 1000
+      // );
 
-      const endDate = new Date(
-        subscriptionData.current_end * 1000
-      );
+      // const endDate = new Date(
+      //   subscriptionData.current_end * 1000
+      // );
 
       // Cancel old active subscription
       await prisma.subscription.updateMany({
@@ -964,8 +964,8 @@ const razorpayWebhook = async (req, res) => {
         },
         data: {
           status: "ACTIVE",
-          startDate,
-          endDate,
+          // startDate,
+          // endDate,
         },
       });
     }
